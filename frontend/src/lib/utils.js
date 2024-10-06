c769ec843403e1efccd97a6f4f28c3328993a75e
Ashish Thakur <akt627777@gmail.com>
2024-10-06 23:27:59 +0530

    url change

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -5,8 +5,8 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export let USER_API = 'http://localhost:8003/api/v1/user'
-export let POST_API = 'http://localhost:8003/api/v1/post'
-export let COMMENT_API = 'http://localhost:8003/api/v1/comment'
-export let MESSAGE_API = 'http://localhost:8003/api/v1/message'
-export let BACKEND_PORT = 'http://localhost:8003'
+export let USER_API = 'https://sastsgrame-1.onrender.com/api/v1/user'
+export let POST_API = 'https://sastsgrame-1.onrender.com/api/v1/post'
+export let COMMENT_API = 'https://sastsgrame-1.onrender.com/api/v1/comment'
+export let MESSAGE_API = 'https://sastsgrame-1.onrender.com/api/v1/message'
+export let BACKEND_PORT = 'https://sastsgrame-1.onrender.com'
